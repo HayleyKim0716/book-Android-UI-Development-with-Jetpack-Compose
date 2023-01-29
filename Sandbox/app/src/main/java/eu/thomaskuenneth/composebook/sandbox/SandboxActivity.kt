90a6da85e22b5f1046ee95e9a48f3d46482a5af0
HayleyKim0716 <hl.kim@dramancompany.com>
2023-01-29 14:26:55 +0900

    chapter04. SandboxActivity 에 ColumnWithTexts() 추가

--- Sandbox/app/src/main/java/eu/thomaskuenneth/composebook/sandbox/SandboxActivity.kt
+++ Sandbox/app/src/main/java/eu/thomaskuenneth/composebook/sandbox/SandboxActivity.kt
@@ -3,12 +3,34 @@ package eu.thomaskuenneth.composebook.sandbox
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
 
 class SandboxActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             // invoke your composable functions here
+        }
+    }
+
+    @Composable
+    @Preview
+    fun ColumnWithTexts() {
+        Column {
+            Text(
+                text = "Android UI development with Jetpack Compose",
+                style = MaterialTheme.typography.h3
+            )
+            Text(
+                text = "Hello Compose",
+                style = MaterialTheme.typography.h5.merge(TextStyle(color = Color.Red))
+            )
         }
     }
 }
